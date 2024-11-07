cb6172b7ed28788b4206371504e95ca6df08b875
vasilikalenchyts <vasilijkalencic@gmail.com>
2024-11-07 21:18:56 +0300

    created file app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 txt = "I’m planning to learn git conflicts resolution."
 print(txt)
+name = input("Please enter your name:")
+surname = input("Please enter your surname:")
+print("Hi, {} {}!".format(name, surname))
